dc829590b704677d90b97283c98587eeedef64c6
Jenkins User <jenkins@cloud.mobilesdp.com>
2011-02-04 15:53:44 +0100

    Updated commit message

--- env.sh
+++ env.sh
@@ -3,4 +3,5 @@ export PATH=$PATH:$EC2_HOME/bin
 export JAVA_HOME=/usr/java/default
 export EC2_PRIVATE_KEY=~/.ssh/pk-DYGAE4U63GGGYPXZQ4DCLWKD3IDGXAVM.pem
 export EC2_CERT=~/.ssh/cert-DYGAE4U63GGGYPXZQ4DCLWKD3IDGXAVM.pem
+export EC2_URL=https://ec2.us-east-1.amazonaws.com
 
