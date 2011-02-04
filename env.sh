7dc55859d54be53993aaada2b35fe7331d3865a8
Jenkins User <jenkins@cloud.mobilesdp.com>
2011-02-04 15:51:19 +0100

    Addded environments file

---
+++ env.sh
@@ -0,0 +1,6 @@
+export EC2_HOME=/usr/local/ec2/apitools
+export PATH=$PATH:$EC2_HOME/bin
+export JAVA_HOME=/usr/java/default
+export EC2_PRIVATE_KEY=~/.ssh/pk-DYGAE4U63GGGYPXZQ4DCLWKD3IDGXAVM.pem
+export EC2_CERT=~/.ssh/cert-DYGAE4U63GGGYPXZQ4DCLWKD3IDGXAVM.pem
+
